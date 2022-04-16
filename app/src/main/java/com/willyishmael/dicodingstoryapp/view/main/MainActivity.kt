d8d23cb783dbe483abc3aa6e7980b363de2b10d4
willyishmael <wtangka22@gmail.com>
2022-04-16 14:46:52 +0800

    Update MainActivity

--- app/src/main/java/com/willyishmael/dicodingstoryapp/view/main/MainActivity.kt
+++ app/src/main/java/com/willyishmael/dicodingstoryapp/view/main/MainActivity.kt
@@ -1,17 +1,46 @@
 package com.willyishmael.dicodingstoryapp.view.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.willyishmael.dicodingstoryapp.R
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.willyishmael.dicodingstoryapp.data.local.UserPreference
 import com.willyishmael.dicodingstoryapp.databinding.ActivityMainBinding
+import com.willyishmael.dicodingstoryapp.view.ViewModelFactory
+import com.willyishmael.dicodingstoryapp.view.login.LoginActivity
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "current_user")
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val pref = UserPreference.getInstance(dataStore)
+        mainViewModel = ViewModelProvider(this, ViewModelFactory(pref))[MainViewModel::class.java]
+
+        checkLoginState()
+    }
+
+    /**
+     * Will direct to Login Activity if loginState is false
+     */
+    private fun checkLoginState() {
+        mainViewModel.getLoginState().observe(this) { loginState ->
+            if (!loginState) {
+                Intent(this@MainActivity, LoginActivity::class.java).apply {
+                    startActivity(this)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/willyishmael/dicodingstoryapp/view/main/MainViewModel.kt
+++ app/src/main/java/com/willyishmael/dicodingstoryapp/view/main/MainViewModel.kt
@@ -1,8 +1,14 @@
 package com.willyishmael.dicodingstoryapp.view.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.willyishmael.dicodingstoryapp.data.local.UserPreference
 
 class MainViewModel (private val pref: UserPreference) : ViewModel() {
+
+    fun getLoginState(): LiveData<Boolean> {
+        return pref.getLoginState().asLiveData()
+    }
 
 }
