debe6c2cdfb789150e0a58eb9269d1e00fd67958
willyishmael <wtangka22@gmail.com>
2022-04-22 02:30:58 +0800

    custom view (still buggy)

---
+++ app/src/main/java/com/willyishmael/dicodingstoryapp/view/custom/PasswordEditText.kt
@@ -0,0 +1,51 @@
+package com.willyishmael.dicodingstoryapp.view.custom
+
+import android.content.Context
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+import com.willyishmael.dicodingstoryapp.R
+
+class PasswordEditText: TextInputEditText {
+
+    private fun init() {
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) if (s.count() < 6) {
+                    this@PasswordEditText.error = context.getString(R.string.password_too_short)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // do nothing
+            }
+        })
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet
+    ) : super(context, attrs) {
+        init()
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+}
