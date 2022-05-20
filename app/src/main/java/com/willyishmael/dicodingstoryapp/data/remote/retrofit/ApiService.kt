c9fa8211a7f120e92142d5c6296a64a55aa7fd75
William Tangka <wtangka22@gmail.com>
2022-05-20 15:53:29 +0800

    Update ApiService.kt

--- app/src/main/java/com/willyishmael/dicodingstoryapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/willyishmael/dicodingstoryapp/data/remote/retrofit/ApiService.kt
@@ -39,7 +39,7 @@ interface ApiService {
         @Query("page") page: Int? = 0,
         @Query("size") size: Int? = 10,
         @Query("location") location: Int? = 0
-    ): Call<GetStoriesResponse>
+    ): GetStoriesResponse
 
     @GET("stories")
     fun getStoriesWithLocation(
